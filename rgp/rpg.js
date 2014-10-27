b86f264b5a9882059fdaeacb3e435d9f093845f0
Rachel Rivera <rachelriv94@yahoo.com>
2014-10-27 08:40:25 -0600

    fix slowness bug

--- rgp/rpg.js
+++ rgp/rpg.js
@@ -2,6 +2,7 @@ $(function() {
     $.getJSON(
         "http://lmu-diabolical.appspot.com/characters",
         function (characters) {
+            console.log("initial GET with characters " + characters);
             $("tbody").append(characters.map(function (character) {
                 var tr = $(".character-template").clone();
                 tr.find(".name").text(character.name);
@@ -12,21 +13,5 @@ $(function() {
             }));
         }
     );
-})
-
-$(function() {
-    $.getJSON(
-        "http://lmu-diabolical.appspot.com/characters",
-        function (characters) {
-            $("tbody").append(characters.map(function (character) {
-                var tr = $(".character-template").clone();
-                tr.find(".name").text(character.name);
-                tr.find(".class").text(character.classType);
-                tr.find(".gender").text(character.gender);
-                tr.find(".level").text(character.level);
-                return tr;
-            }));
-        }
-    );
-})
+});
 
