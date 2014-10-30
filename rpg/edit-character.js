70477e590ba10058f7459d23bf99650350fe3356
Rachel Rivera <rachelriv94@yahoo.com>
2014-10-30 08:23:41 -0600

    reformat template

---
+++ rpg/edit-character.js
@@ -0,0 +1,9 @@
+$('#change-name').click(function() {
+    var searchField = $('#search').val();
+    var searchRegExp = new RegExp(searchField, "i");
+    $(".character-template").each(function(index, row){
+        var allCells = $(row).find('td');
+        var nameCell = allCells[0];
+        ($(nameCell).text().search(searchRegExp) !== -1) ? $(row).show() : $(row).hide();
+    });
+});
--- rpg/rpg.js
+++ rpg/rpg.js
@@ -5,42 +5,29 @@ $(function() {
         function (characters) {
             console.log("initial GET with characters " + JSON.stringify(characters));
             $("tbody").append(characters.map(function (character) {
-                var filledInTemplate = fillInTemplate(template, character);
+                var filledInTemplate = fillInCharacterInfo(".character-template", character);
                 filledInTemplate.find(".edit").bind("click", function(){
                     showEditCharacterModal(character);
                 });
                 filledInTemplate.find(".delete").bind("click", function(){
                     showDeleteCharacterModal(character);
                 })
-                return tr;
+                return filledInTemplate;
             }));
         }
     );
 
-    var fillInTemplate = function(template, character) {
-        var filledInTemplate = $(template).clone();
-        filledInTemplate.find(".name").text(character.name);
-        filledInTemplate.find(".class").text(character.classType);
-        filledInTemplate.find(".gender").text(character.gender);
-        filledInTemplate.find(".level").text(character.level);
-        return filledInTemplate;
-    }
-
     var showEditCharacterModal = function(character) {
-        genderSpecificTemplate = ".edit-character-template-male"
-        // genderSpecificTemplate = ".edit-character-template-" + character.gender.toLowerCase();
-        var filledOutTemplate = fillInTemplate(genderSpecificTemplate, character);
+        var genderIcon = ".character-icon-" + character.gender.toLowerCase();
+        console.log("gender icon " + genderIcon);
+        var filledInTemplate = fillInCharacterInfo(genderIcon, character)
+        templateWithIcon = $(".character-icon").add($(filledInTemplate));
+        $(".edit-character-template").find(".character-icon").replaceWith( $(templateWithIcon) );
         BootstrapDialog.show({
             type: BootstrapDialog.TYPE_INFO,
             title: 'Edit Character ' + character.name,
-            message: $(filledOutTemplate),
-            buttons: [
-                {
-                label: 'Save All Changes',
-                cssClass: 'btn-success',
-                // action: saveNewInfo()
-                },
-                {
+            message: $(".edit-character-template"),
+            buttons: [{
                 label: 'Cancel',
                 cssClass: 'btn-danger',
                 action: function(dialogueItself){
@@ -50,5 +37,13 @@ $(function() {
         })
     }
 
-    $("#change-name").click
+    var fillInCharacterInfo = function(template, character) {
+        var filledInTemplate = $(template).clone();
+        filledInTemplate.find(".name").text(character.name);
+        filledInTemplate.find(".class").text(character.classType);
+        filledInTemplate.find(".gender").text(character.gender.toLowerCase());
+        filledInTemplate.find(".level").text(character.level);
+        return filledInTemplate;
+    }
+
 });
