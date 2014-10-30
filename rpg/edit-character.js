964487494eec78262090b269643a31ce806767f3
Rachel Rivera <rachelriv94@yahoo.com>
2014-10-30 10:00:23 -0600

    PUT for characters

--- rpg/edit-character.js
+++
@@ -1,9 +0,0 @@
-$('#change-name').click(function() {
-    var searchField = $('#search').val();
-    var searchRegExp = new RegExp(searchField, "i");
-    $(".character-template").each(function(index, row){
-        var allCells = $(row).find('td');
-        var nameCell = allCells[0];
-        ($(nameCell).text().search(searchRegExp) !== -1) ? $(row).show() : $(row).hide();
-    });
-});
